65f1e908b72f153921ddd96fb2528fa3188c6c90
user2 <user2@user2-PC>
2019-12-23 20:10:38 +0900

    1223jieun

--- semi/src/qna/model/dao/QnaDao.java
+++ semi/src/qna/model/dao/QnaDao.java
@@ -16,6 +16,7 @@ import java.util.Properties;
 import attachment.Attachment;
 import notice.model.vo.Notice;
 import qna.model.vo.Qna;
+import qna.model.vo.QnaRe;
 
 
 public class QnaDao {
@@ -291,6 +292,64 @@ public class QnaDao {
 		}
 		
 		return result;
+	}
+
+	// 댓글 입력
+	public int insertReply(Connection conn, QnaRe r) {
+		PreparedStatement pstmt = null;
+		
+		int result = 0;
+		
+		String sql = prop.getProperty("insertReply");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, r.getRqComment());
+			pstmt.setString(2, r.getqNo());
+			pstmt.setString(3, r.getcId());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		return result;
+	}
+
+	public ArrayList<QnaRe> selectReplyList(Connection conn, String getqNo) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<QnaRe> rlist = null;
+		
+		String sql = prop.getProperty("selectReplyList");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, getqNo);
+			
+			rs = pstmt.executeQuery();
+			
+			rlist = new ArrayList<QnaRe>();
+			
+			while(rs.next()) {
+				rlist.add(new QnaRe(rs.getString("rq_no"),
+									rs.getString("rq_Comment"),
+									rs.getDate("rq_EntDate"),
+									rs.getString("rq_Secret"),
+									rs.getString("cq_no2"),
+									rs.getString("cId"),
+									rs.getString("rq_chk")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		return rlist;
 	}
 	
 }
